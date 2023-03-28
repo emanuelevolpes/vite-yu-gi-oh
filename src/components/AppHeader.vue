4cd5233614d9166a105455e7feef3a1a5652c407
emanuelevolpes <emanuele.volpes1@gmail.com>
2023-03-28 14:47:45 +0200

    header

--- src/components/AppHeader.vue
+++ src/components/AppHeader.vue
@@ -6,6 +6,13 @@ export default {
 
 <template>
     <header>
+        <nav class="navbar navbar-light bg-light px-3">
+            <a class="navbar-brand" href="#">
+                <img src="../../public/img/logo.png" width="50" height="30"
+                    class="d-inline-block align-top" alt="">
+                Yu-Gi-Oh API
+            </a>
+        </nav>
     </header>
 </template>
 
