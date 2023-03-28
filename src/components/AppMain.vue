835569d7f1088566fb142cde0940d3edf3e7d8f6
emanuelevolpes <emanuele.volpes1@gmail.com>
2023-03-28 16:40:04 +0200

    dropdown

--- src/components/AppMain.vue
+++ src/components/AppMain.vue
@@ -9,7 +9,17 @@ export default {
 </script>
 
 <template>
-    <main>
+    <main class="d-flex flex-column">
+        <div class="container-sm dropdown mb-5">
+            <button class="btn dropdown-toggle" type="button" data-bs-toggle="dropdown" aria-expanded="false">
+                Select type
+            </button>
+            <ul class="dropdown-menu">
+                <li><a class="dropdown-item" href="#">Action</a></li>
+                <li><a class="dropdown-item" href="#">Another action</a></li>
+                <li><a class="dropdown-item" href="#">Something else here</a></li>
+            </ul>
+        </div>
         <div class="container d-flex">
             <span>Found 39 cards</span>
             <CardList />
@@ -21,6 +31,11 @@ export default {
 main {
     background-color: #d48f38;
     padding: 50px;
+
+    button {
+        background-color: white;
+    }
+
     .container {
         background-color: white;
         padding: 30px;
